d6bc3c034330df94ee8c05a1d46a53dbd2594ebb
UnnamedMistress <momchrysti@gmail.com>
2023-01-03 15:58:21 -0500

    created new quiz format

--- style/script.js
+++ style/script.js
@@ -1,145 +1,194 @@
 
-const question = document.getElementById('question');
-const src = document.getElementById('imgDemo');
-const a = document.getElementById('a+');
-const b = document.getElementById('b+');
-const c = document.getElementById('c+');
-const d = document.getElementById('d+');
-const btn = document.getElementById('submit');
-const all_answer = document.querySelectorAll('.answer');
-const quiz = document.getElementById('quiz');
-const start = document.getElementById('start');
-
-btn.hidden = true;
-var timerEl = document.getElementById('countdown');
-var mainEl = document.getElementById('main');
-
-function startCountDown() {
-let timeLeft = 120;
-let timeInterval = setInterval(function () {
-    if (timeLeft > 0) {
-        timerEl.textContent = timeLeft + ' remaining';
-        timeLeft--; 
-    }
-    else {
-        timerEl.textContent = 'Time is up';
-        clearInterval(timeInterval);
-    }
-}, 1000);
-}
-start.addEventListener('click', function () {
-    startCountDown();})
-
-const quizData = [
+let timeLeft = document.querySelector(".time-left");
+let quizContainer = document.getElementById("container");
+let nextBtn = document.getElementById("next-button");
+let countOfQuestion = document.querySelector(".number-of-question");
+let displayContainer = document.getElementById("display-container");
+let scoreContainer = document.querySelector(".score-container");
+let restart = document.getElementById("restart");
+let userScore = document.getElementById("user-score");
+let startScreen = document.querySelector(".start-screen");
+let startButton = document.getElementById("start-button");
+let questionCount;
+let scoreCount = 0;
+let count = 11;
+let countdown;
+const quizArray = [
     {
-      src: ("./array.png"),  
+      id:"0",  
+      src: "style/img/array.png",  
       question: 'How do you sort an Array?',
-      a: 'By pressing the sort button in the tool bar',
-      b: 'sort();',
-      c: 'Just use filter instead',
-      d: 'Math.min.apply(null, [1, 2, 3])',
-      correct: 'b',
+      options:['By pressing the sort button in the tool bar','sort()','Just use filter instead','Math.min.apply(null, [1, 2, 3])'],
+      correct: 'sort();',
     },
     {
-        src: "/Classes.png",  
+        id:"1", 
+        src: "style/img/Classes.png",  
         question: 'What is the difference between Class and ID?',
-        a: 'Class can be used by multiple HTML elements, while an ID is used by one HTML element within the page',
-        b: 'A class is what you attend in school while an ID is the name tag you wear while your in class',
-        c: 'While an ID can be used by multiple HTML elements, a class can only be used once',
-        d: 'While Alive { eat (); sleep (); code (); I seem to have gotten lost',
-        correct: 'a',
+        options: ['Class can be used by multiple HTML elements, while an ID is used by one HTML element within the page', 'A class is what you attend in school while an ID is the name tag you wear while your in class','While an ID can be used by multiple HTML elements, a class can only be used once', 'While Alive { eat (); sleep (); code (); I seem to have gotten lost'],
+        correct: 'Class can be used by multiple HTML elements, while an ID is used by one HTML element within the page',
     },
     {
-        src: "/closing",  
+        id:"2",
+        src: "style/img/closing",  
         question: 'Which tag does NOT need a closing tag',
-        a: '<li>',
-        b: '<HTML>',
-        c: '<br>',
-        d: '<p>',
-        correct: 'c',
+        options:[ '<li>','<HTML>','<br>','<p>'],
+        correct: '<li>',
     },
-    {
-        src: "/float",  
+    {   id:"3",
+        src: "style/img/float",  
         question: 'What is a common problem with using Float in CSS?',
-        a: 'Float only works on an absolute or fixed position',
-        b: 'You do not have any life boats if your floatation devise fails',
-        c: 'The siblings of that Float will be unaffected',
-        d: 'A common problem with float-based layouts is that the floats container does not want to stretch up to accommodate the floats',
-        correct: 'd',
+        options:[ 'Float only works on an absolute or fixed position','You do not have any life boats if your floatation devise fails','The siblings of that Float will be unaffected','A common problem with float-based layouts is that the floats container does not want to stretch up to accommodate the floats'],
+        correct: 'A common problem with float-based layouts is that the floats container does not want to stretch up to accommodate the floats',
     },
-    {
-        src: "/function.png",  
+    {   id:"4",
+        src: "style/img/function.png",  
         question: 'There is only one way to write a Function',
-        a: 'True',
-        b: 'False',
-        correct: 'b',
+        options: ['True','False'],
+        correct: 'False',
     },
-    {
-        src: "/Git.png",  
+    {   id:"5",
+        src: "style/img/Git.png",  
         question: 'What is Git used for?',
-        a: 'Breaking things',
-        b: 'Git is just another word for Get',
-        c: 'Version Control',
-        d: 'Group Therapy',
-        correct: 'c',
-    },   {
-        src: "/loop.png",  
+        options: ['Breaking things', 'Git is just another word for Get','Version Control','Group Therapy'],
+        correct: 'Version Control',
+    },  
+    {   id:"6",
+        src: "style/img/loop.png",  
         question: 'How many times will Barney get kicked out of Moes?',
-        a: 'Whenever Moe gets tired',
-        b: 'When Barney runs out of money',
-        c: 'When the criteria is met, somehow',
-        d: 'Indefinitely, the loop will go on forever',
-        correct: 'd',
+       options:['Whenever Moe gets tired','When Barney runs out of money','When the criteria is met, somehow','Indefinitely, the loop will go on forever'],
+        correct: 'Indefinitely, the loop will go on forever',
     },
-    {
-        src: "/nested.png",  
-        question: 'What kind of Nest is this?',
-        a: 'A virtual one???',
-        b: 'A nested IF statement',
-        c: 'A nested Function',
-        d: 'Do not call it a nest, it is a HOME!',
-        correct: 'b',
-    },
-    {
-        src: "/recursion.png",  
-        question: 'What is Recursion?',
-        a: 'Recursion is when a function calls itself until someone stops it.',
-        b: 'Recursion is when your boyfriend calls his ex, until you put a stop to it',
-        c: 'Recursion is when a waitress at Olive Garden grates cheese over your pasta until someone else at the table stops it',
-        d: 'Recursion is when a you keep pressing snooze on your alarm clock',
-        correct: 'a',
-    },
-    {
-        src: "/var.png",  
-        question: 'Which type cannot be reassigned?',
-        a: 'var',
-        b: 'const',
-        c: 'let',
-        d: 'dum',
-        correct: 'b',
-    },
+   
  ];
-
-let QIndex = 0;
-
-function startQuiz () {
- question.innerHTML = quizData[i].question;
-   imgDemo.innerHTML = quizData[i].src;
-   a.innerHTML = quizData[i].a;
-   b.innerHTML = quizData[i].b;
-   c.innerHTML = quizData[i].c;
-    d.innerHTML = quizData[i].d;
-    QIndex++;
-    btn.hidden = false;
-}
-start.onclick= startQuiz;
-quizData.foreach(startQuiz)
-
-
-// if statements to know if answer is right
- //Create counter for score and append to score at the top of page
- //Create end Quiz function
-// Request name
-// create High Scores page with list of scores and names
-//link to HighScores at the 'end quiz' function
-//Create function to ask to play again
+ 
+//Restart Quiz
+restart.addEventListener("click", () => {
+    initial();
+    displayContainer.classList.remove("hide");
+    scoreContainer.classList.add("hide");
+  });
+  //Next Button
+  nextBtn.addEventListener(
+    "click",
+    (displayNext = () => {
+      //increment questionCount
+      questionCount += 1;
+      //if last question
+      if (questionCount == quizArray.length) {
+        //hide question container and display score
+        displayContainer.classList.add("hide");
+        scoreContainer.classList.remove("hide");
+        //user score
+        userScore.innerHTML =
+          "Your score is " + scoreCount + " out of " + questionCount;
+      } else {
+        //display questionCount
+        countOfQuestion.innerHTML =
+          questionCount + 1 + " of " + quizArray.length + " Question";
+        //display quiz
+        quizDisplay(questionCount);
+        count = 11;
+        clearInterval(countdown);
+        timerDisplay();
+      }
+    })
+  );
+//Timer
+const timerDisplay = () => {
+    countdown = setInterval(() => {
+      count--;
+      timeLeft.innerHTML = `${count}s`;
+      if (count == 0) {
+        clearInterval(countdown);
+        displayNext();
+      }
+    }, 1000);
+  };
+  //Display quiz
+  const quizDisplay = (questionCount) => {
+    let quizCards = document.querySelectorAll(".container-mid");
+    //Hide other cards
+    quizCards.forEach((card) => {
+      card.classList.add("hide");
+    });
+    //display current question card
+    quizCards[questionCount].classList.remove("hide");
+  };
+  //Quiz Creation
+  function quizCreator() {
+    //randomly sort questions
+    quizArray.sort(() => Math.random() - 0.5);
+    //generate quiz
+    for (let i of quizArray) {
+      //randomly sort options
+      i.options.sort(() => Math.random() - 0.5);
+      //quiz card creation
+      let div = document.createElement("div");
+      div.classList.add("container-mid", "hide");
+      //question number
+      countOfQuestion.innerHTML = 1 + " of " + quizArray.length + " Question";
+      //question
+      let question_DIV = document.createElement("p");
+      question_DIV.classList.add("question");
+      question_DIV.innerHTML = i.question;
+      question_DIV.innerHTML = `<img src=${i.src} alt="picture" width = "200px" height="200px" />`;
+      div.appendChild(question_DIV);
+      //options
+      div.innerHTML += `
+      <button class="option-div" onclick="checker(this)">${i.options[0]}</button>
+       <button class="option-div" onclick="checker(this)">${i.options[1]}</button>
+        <button class="option-div" onclick="checker(this)">${i.options[2]}</button>
+         <button class="option-div" onclick="checker(this)">${i.options[3]}</button>
+      `;
+      quizContainer.appendChild(div);
+    }
+  }
+  //Checker Function to check if option is correct or not
+  function checker(userOption) {
+    let userSolution = userOption.innerText;
+    let question =
+      document.getElementsByClassName("container-mid")[questionCount];
+    let options = question.querySelectorAll(".option-div");
+    //if user clicked answer == correct option stored in object
+    if (userSolution === quizArray[questionCount].correct) {
+      userOption.classList.add("correct");
+      scoreCount++;
+    } else {
+      userOption.classList.add("incorrect");
+      //For marking the correct option
+      options.forEach((element) => {
+        if (element.innerText == quizArray[questionCount].correct) {
+          element.classList.add("correct");
+        }
+      });
+    }
+    //clear interval(stop timer)
+    clearInterval(countdown);
+    //disable all options
+    options.forEach((element) => {
+      element.disabled = true;
+    });
+  }
+  //initial setup
+  function initial() {
+    quizContainer.innerHTML = "";
+    questionCount = 0;
+    scoreCount = 0;
+    count = 11;
+    clearInterval(countdown);
+    timerDisplay();
+    quizCreator();
+    quizDisplay(questionCount);
+  }
+  //when user click on start button
+  startButton.addEventListener("click", () => {
+    startScreen.classList.add("hide");
+    displayContainer.classList.remove("hide");
+    initial();
+  });
+  //hide quiz and display start screen
+  window.onload = () => {
+    startScreen.classList.remove("hide");
+    displayContainer.classList.add("hide");
+  };
